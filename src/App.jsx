9a7a9cd9b12f43011f745a9e62f5d6685516da81
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-03-03 15:08:37 +0530

    removed default styling and created the starter code for the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
-      App Div
+      <Navbar />
     </div>
   )
 }
