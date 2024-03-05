fa4ce260cc83cf4a7c73723e53771838d9723b53
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-03-05 14:19:32 +0530

    added images to the carousel that will cycle through the display

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+// Library or page imports
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+// Style imports
 import styles from './Carousel.module.css';
 
-export default function Carousel({ images }) {
+export default function Carousel({ images, interval = 3000 }) {
     const [activeIdx, setActiveIdx] = useState(0);
     const nextSlide = () => {
         setActiveIdx(prevIdx => prevIdx === images.length - 1 ? 0 : prevIdx + 1);
@@ -9,6 +13,15 @@ export default function Carousel({ images }) {
     const prevSlide = () => {
         setActiveIdx(prevIdx => prevIdx === 0 ? images.length - 1 : prevIdx - 1);
     }
+
+    // autoplay functionality - defaulted to 3000 ms (3 seconds)
+    useEffect(() => {
+        const autoPlayInterval = setInterval(nextSlide, interval);
+        return () => {
+            clearInterval(autoPlayInterval);
+        };
+    },[interval]);
+
   return (
     <div className={styles.carousel}>
         <img className={styles['carousel-img']} src={images[activeIdx]} alt="carousel" />
@@ -18,4 +31,9 @@ export default function Carousel({ images }) {
         </div>
     </div>
   )
+}
+
+Carousel.propTypes = {
+    images: PropTypes.array,
+    interval: PropTypes.number
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,9 +7,11 @@ import styles from './Home.module.css';
 export default function Home() {
 
     const carouselImages = [
-        'https://via.placeholder.com/800x400/ff5733/fff',
-        'https://via.placeholder.com/800x400/33ff57/fff',
-        'https://via.placeholder.com/800x400/5733ff/fff',
+        'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/carousel%2Fwho_accolade.jpg?alt=media&token=fbea4c5d-aab1-4486-bed7-88e242295eec',
+        'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/carousel%2Fadmission.jpeg?alt=media&token=c17b88b9-0df3-4b31-bcfb-839161b68391',
+        'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/carousel%2Frlist-ot.jpg?alt=media&token=070f4c29-25e6-4272-bcf4-b6b97a82aacd',
+        'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/carousel%2Fnotif1.jpeg?alt=media&token=2b71acb0-7609-4bba-a417-ad1d0fd121ca',
+        'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/carousel%2FSW%20DAY%202022.jpg?alt=media&token=2a8561d9-b82b-4e54-8440-fd4199644814'
     ];
 
   return (
