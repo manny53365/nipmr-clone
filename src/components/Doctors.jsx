3cb519579b9d44c69069d122ae8a6a06bf77ad8c
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-03-05 15:07:40 +0530

    added some styling and mapped the doctors to the dom

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,9 +1,23 @@
+// library or page imports
+import PropTypes from 'prop-types';
+
+// style imports
 import styles from './Doctors.module.css';
 
-export default function Doctors() {
+export default function Doctors({ doctors }) {
   return (
-    <div>
-        Doctors
+    <div className={styles.doctors}>
+        {doctors.map(doctor => (
+            <div key={doctor.name} className={styles.doctor}>
+                <img src={doctor.image} alt="Doctor" />
+                <h3>{doctor.name}</h3>
+                <p>{doctor.title}</p>
+            </div>
+        ))}
     </div>
   )
+}
+
+Doctors.propTypes = {
+    doctors: PropTypes.array
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 // Page or component imports
 import Carousel from '../../components/Carousel';
+import Doctors from '../../components/Doctors';
 
 // Style imports
 import styles from './Home.module.css';
@@ -14,6 +15,34 @@ export default function Home() {
         'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/carousel%2FSW%20DAY%202022.jpg?alt=media&token=2a8561d9-b82b-4e54-8440-fd4199644814'
     ];
 
+    const othDoctors = [
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/oth-doctors%2FPinarayi%20Vijayan.png?alt=media&token=c0d9b88c-e060-4341-9f1a-4520b28b4195',
+            name: 'Shri. Pinarayi Vijayan',
+            title: 'Chief Minister'
+        },
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/oth-doctors%2FDr-R-Bindhu.jpeg?alt=media&token=b215fc14-f469-404b-9263-2f1bddec2005',
+            name: 'Dr. R Bindu',
+            title: 'Minister of Higher Education and Social Justice'
+        },
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/oth-doctors%2Fpuneet-kumar-ias.png?alt=media&token=23f58198-5b61-436c-af8c-803de2a04ad9',
+            name: 'Sri. Puneet Kumar, IAS',
+            title: 'Additional Chief Secretary, Social Justice Department'
+        },
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/oth-doctors%2FDINESHAN%20H%20IAS.jpg?alt=media&token=b4f31364-b779-46a5-844e-e964f6e5b880',
+            name: 'Sri. H. Dineshan IAS',
+            title: 'Director, Social Justice Department'
+        },
+        {
+            image: 'https://firebasestorage.googleapis.com/v0/b/nipmr-app.appspot.com/o/oth-doctors%2FSri%20Chandrababu%20C.png?alt=media&token=18cf890d-9237-441a-a821-b839b01b41af',
+            name: 'Sri. Chandrababu C',
+            title: 'Executive Director'
+        }
+    ];
+
   return (
     <div className={styles['home-container']}>
         <div className={styles['main-container']}>
@@ -21,7 +50,7 @@ export default function Home() {
                 <Carousel images={carouselImages}/>
             </div>
             <div className={styles['right-main']}>
-
+                <Doctors doctors={othDoctors}/>
             </div>
         </div>
     </div>
